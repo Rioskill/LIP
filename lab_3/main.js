38ec892d2aac60f4472200487bbe568d70f56b09
Nikita <rioskill@yandex.ru>
2022-09-22 22:45:49 +0300

    changed regular spaces to nbsp

--- lab_3/main.js
+++ lab_3/main.js
@@ -5,19 +5,19 @@ function printElement (element, nesting=0) {
 
     let nestingString = '';
     if (nesting > 0) {
-        nestingString = ' '.repeat(nesting * 2) + '|_';
+        nestingString = '\u00A0'.repeat(nesting * 2) + '|_';
     }
 
     let result = nestingString + element.tagName + '\n';
 
-    for(let child of element.children)
+    for(const child of element.children)
         result += printElement(child, nesting + 1);
 
     return result;
 }
 
 
-let string = printElement(document.body);
+const string = printElement(document.body);
 console.log(string);
 let container = document.getElementById("printer");
 container.innerText = string;
