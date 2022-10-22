a40ae40753a292e40d75899f745d1d0ebeed7f86
nikstarling <rioskill@yandex.ru>
2022-10-22 14:16:24 +0300

    renamed tests

--- lab_5/part_1/tests.rb
+++ lab_5/part_1/tests.rb
@@ -5,12 +5,12 @@ require_relative 'calculator'
 
 # tests calc method
 class CalcTest < Minitest::Test
-  def test_sin_of_one
+  def test_one
     result = Calculator.calc(1)
     assert_in_delta(result, -0.38542559176909813, 1e-17)
   end
 
-  def test_sin_of_four
+  def test_four
     result = Calculator.calc(4)
     assert_in_delta(result, 0.6639225799939713, 1e-17)
   end
